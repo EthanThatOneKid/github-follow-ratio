874c18456498536268e69acadbed4e25ff9bd956
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2024-06-22 14:30:59 -0700

    add `@fartlabs/rt`

--- main.ts
+++ main.ts
@@ -1,27 +1,30 @@
 import { MINUTE } from "@std/datetime";
+import { createRouter } from "@fartlabs/rt";
 import type { FollowRatio } from "./github.ts";
 import { getFollowRatio } from "./github.ts";
 
 const kv = await Deno.openKv();
 
-Deno.serve(async (request) => {
-  const url = new URL(request.url);
-  const username = url.searchParams.get("username");
-  if (!username) {
-    return new Response("Missing username", { status: 400 });
-  }
+const router = createRouter()
+  .get<"username">("/api/:username", async (event) => {
+    const { username } = event.params;
+    if (!username) {
+      return new Response("Missing username", { status: 400 });
+    }
 
-  const followRatioKey: Deno.KvKey = ["followRatio", username];
-  const followRatioResult = await kv.get<FollowRatio>(followRatioKey);
-  if (followRatioResult?.value) {
-    return Response.json(followRatioResult.value);
-  }
+    const followRatioKey: Deno.KvKey = ["followRatio", username];
+    const followRatioResult = await kv.get<FollowRatio>(followRatioKey);
+    if (followRatioResult?.value) {
+      return Response.json(followRatioResult.value);
+    }
 
-  const followRatio = await getFollowRatio(username);
-  await kv.set(
-    followRatioKey,
-    followRatio,
-    { expireIn: 5 * MINUTE },
-  );
-  return Response.json(followRatio);
-});
+    const followRatio = await getFollowRatio(username);
+    await kv.set(
+      followRatioKey,
+      followRatio,
+      { expireIn: 5 * MINUTE },
+    );
+    return Response.json(followRatio);
+  });
+
+Deno.serve((request) => router.fetch(request));
